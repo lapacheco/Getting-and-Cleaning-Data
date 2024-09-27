258fb694fdd948cb9a8001103c25b24f028bacbb
lapacheco <luis_pacheco@me.com>
2024-09-27 12:58:16 -0700

    Update gettingCleaningData.R

--- gettingCleaningData.R
+++ gettingCleaningData.R
@@ -1,1 +1,14 @@
 
+## connect to MySQL
+ucscDb<- dbConnect(MySQL(),user="genome",host="geome-mysql.cse.ucsc.edu")
+## show all dabases in the server
+result<-dbGetQuery(ucscDb,"show databases;"); dbDisconnect(ucscDb);
+
+## connect to a table
+hg19<- dbConnect(MySQL(),user="genome", db="hg19" ,host="geome-mysql.cse.ucsc.edu")
+allTables<- dbListTables(hg19)
+
+## list fields
+hg19Fields<- dbListFields(hg19,"tablename")
+## run a query
+hg19Query <- (hg19, "select count(*) from tablename")
